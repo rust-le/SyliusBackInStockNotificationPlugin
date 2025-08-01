20bbcbe64a1da81fbd32e8365ced0ee09b4a7549
Maxime <maxime@zangra.com>
2025-08-01 11:40:56 +0200

    Forget to add the use AsCommand

--- src/Command/AlertCommand.php
+++ src/Command/AlertCommand.php
@@ -10,6 +10,7 @@ use Sylius\Component\Channel\Model\ChannelInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 use Sylius\Component\Inventory\Checker\AvailabilityCheckerInterface;
 use Sylius\Component\Mailer\Sender\SenderInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -36,14 +37,6 @@ final class AlertCommand extends Command
         private readonly RouterInterface $router,
     ) {
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->setDescription('Send an email to the user if the product is returned in stock')
-            ->setHelp('Check the stock status of the products in the webgriffe_back_in_stock_notification table and send and email to the user if the product is returned in stock')
-        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
